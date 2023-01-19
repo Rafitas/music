afc77b95f0f9c0624da508c593bf374db3ef09be
Evelyn Rafaela Suquinagua Loja <rafaela.suquinagua@ucuenca.edu.ec>
2023-01-19 14:00:38 -0500

    Musica en un cuadrado video 1,2,3,4,5

--- sketch.js
+++ sketch.js
@@ -1,18 +1,18 @@
 var song;
 var button;
-// var slider;
+var amp;
 
 
 function setup() {
   createCanvas(200, 200);
-  song = loadSound('flowers.mp3');
-  button = createButton("play");
-  button.mousePressed(togglePlaying);
+  song = loadSound('flowers.mp3', loaded);
+  amp = new p5.Amplitude();
   background(51);
-  // slider = createSlider(0, 1, 0.5, 0.01);
 }
 
 function loaded() {
+  button = createButton("play");
+  button.mousePressed(togglePlaying);
   console.log("loaded");
 }
 
@@ -29,8 +29,14 @@ function togglePlaying() {
 
 }
 
-//FUNCION DE SLIDER//
-// function draw() {
-//   background(random(255));
-  // song.setVolume(slider.value());
-//}
+function draw() {
+  background(255, 0, 255);
+
+  var vol = amp.getLevel();
+  var diam = map(vol, 0, 3, 10, 200);
+
+  fill(255, 0, 255);
+  ellipse(width / 2, height / 2, diam, vol);
+}
+
+
