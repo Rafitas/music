e92ea2ec209f935e4c3168390224fc7c4631e09e
Evelyn Rafaela Suquinagua Loja <rafaela.suquinagua@ucuenca.edu.ec>
2023-01-24 11:15:54 -0500

    Actualizacion codigo

--- sketch.js
+++ sketch.js
@@ -1,42 +1,126 @@
-var song;
-var button;
-var amp;
+var song
+var img
+var fft
+var particles = []
 
+///////////////////////////////////////////////
+///// FUNCITON PRELOAD CARGA DE LA MUSICA /////
+function preload() {
+  song = loadSound('flowers.mp3');
+  img = loadImage('flores.jpg')
+}
 
 function setup() {
-  createCanvas(200, 200);
-  song = loadSound('flowers.mp3', loaded);
-  amp = new p5.Amplitude();
-  background(51);
-}
+  createCanvas(windowWidth, windowHeight);
+  angleMode(DEGREES);
+  imageMode(CENTER);
+  fft = new p5.FFT();
 
-function loaded() {
-  button = createButton("play");
-  button.mousePressed(togglePlaying);
-  console.log("loaded");
-}
+  img.filter(BLUR, 12);
 
-function togglePlaying() {
-  if (!song.isPlaying()) {
-    song.play();
-    song.setVolume(0.3);
-    button.html('pause');
-  } else {
+  noLoop();
+}
+/////////////////////////////////////////////////////////////
+////////// FUNCION DE CLICK PARA REPRODUCIR//////////////////
+
+function mouseClicked() {
+  if (song.isPlaying()) {
     song.pause();
-    button.html("play");
+    noLoop();
+  } else {
+    song.play();
+    loop();
+  }
+}
+
+///////////////////////////////////
+////////// FUNCTION DRAW //////////
+function draw() {
+  background(0);
+  stroke(255, 204, 0);
+  strokeWeight(3);
+  fill('#222222');
+  translate(width / 2, height / 2);
+
+  fft.analyze()
+  amp = fft.getEnergy(20, 200);
+
+  push()
+  if (amp > 250) {
+    rotate(random(-0.5, 0.5))
   }
 
+  image(img, 0, 0, width.height)
+  pop()
+
+  var wave = fft.waveform();
+
+  for (var t = -1; t <= 1; t += 2) {
+    beginShape();
+
+    for (var i = 0; i <= 180; i++) {
+      var index = floor(map(i, 0, 180, 0, wave.length - 1));
+
+      var r = map(wave[index], -1, 1, 150, 350);
+
+      var x = r * sin(i) * t
+      var y = r * cos(i);
+      vertex(x, y);
+
+    }
+    endShape();
+  }
+
+  /////////////////////////////////////////////
+  ////// VARIABLE DE LAS PARTICULAS //////////
+  ////////////////////////////////////////////
+
+  var p = new Particle()
+  particles.push(p)
+
+  for (var i = 0; i < particles.length; i++) {
+    if (!particles[i].edges()) {
+      particles[i].update()
+      particles[i].show()
+    } else {
+      particles.splice(i, 1);
+    }
+  }
 
 }
 
-function draw() {
-  background(255, 0, 255);
 
-  var vol = amp.getLevel();
-  var diam = map(vol, 0, 3, 10, 200);
+/////////////////////////////////////////////////
+////// CLASE PARA LAS PARTICULAS DEL FONDO //////
+/////////////////////////////////////////////////
 
-  fill(255, 0, 255);
-  ellipse(width / 2, height / 2, diam, vol);
+class Particle {
+  constructor() {
+    this.pos = p5.Vector.random2D().mult(250);
+    this.vel = createVector(0, 0);
+    this.acc = this.pos.copy().mult(random(0.0001, 0.00001));
+
+    this.w = random(3, 5);
+
+  }
+  update() {
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+  }
+
+  edges() {
+    if (this.pos.x < -width / 2 || this.pos.x > width / 2 ||
+      this.pos.y < -height / 2 || this.pos.y > height / 2) {
+      return true
+    } else {
+      return false
+    }
+  }
+  show() {
+    stroke(255, 204, 0);
+    fill(255)
+    ellipse(this.pos.x, this.pos.y, 4);
+  }
 }
 
 
